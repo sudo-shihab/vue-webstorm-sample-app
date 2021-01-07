74398ca5d6fcefeb594de121bb07990cbe39d4a7
Shihab <contact.shihab.me@gmail.com>
2021-01-07 20:19:26 +0530

    test husky 22

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,7 @@
     <hero-page-header
       title="About Us Page"
       sub-title="Description about about page bla"
-    />
-        <app-tabs :menu-list="['sports', 'news']" />
+    /><app-tabs :menu-list="['sports', 'news']" />
   </div>
 </template>
 
