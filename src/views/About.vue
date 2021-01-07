0bdf8f3ba485056137c954b31f82da7abc24328e
Shihab <contact.shihab.me@gmail.com>
2021-01-07 19:28:57 +0530

    test husky 3

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <app-nav-bar-wrapper />
     <hero-page-header
       title="About Us Page"
-      sub-title="Description about about page bla bla bla"
+      sub-title="Description about about page bla  bla"
     />
     <app-tabs :menu-list="['partners', 'publishers']" />
   </div>
