f0404a7be98b6fe7f5147e43868f89a10ba71147
Shihab <contact.shihab.me@gmail.com>
2021-01-07 19:31:53 +0530

    test husky 7

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="about">
     <app-nav-bar-wrapper />
+    <app-tabs :menu-list="['sports', 'news']" />
     <hero-page-header
       title="About Us Page"
       sub-title="Description about about page bla"
     />
 
-    <p>shihah</p>
+    <p>shihah fdklvnl fjvklfdklv j jdkl fv</p>
   </div>
 </template>
 
